cf9f6eb387599a73cc11224ac5c300932b0f7855
Giovane <giovaneaalmeida27@gmail.com>
2021-12-21 11:14:46 -0300

    feat: add routes getAllTasks, getTaskById and deleteTask

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { CreateTaskDTO } from './dtos/create-task.dto';
 import { Task } from './tasks.model';
 import { TasksService } from './tasks.service';
@@ -7,7 +7,23 @@ import { TasksService } from './tasks.service';
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
+  @Post()
   async createTask(@Body() createTaskDTO: CreateTaskDTO): Promise<Task> {
     return await this.tasksService.createTask(createTaskDTO);
+  }
+
+  @Get()
+  async getAllTasks(): Promise<Task[]> {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Get('/:id')
+  async getTaskById(@Param('id') id: string): Promise<Task> {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Delete('/:id')
+  async deleteTask(@Param('id') id: string): Promise<void> {
+    return this.tasksService.deleteTask(id);
   }
 }
