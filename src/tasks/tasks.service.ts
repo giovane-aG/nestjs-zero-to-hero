e128e69d35eb7f7614af0b176bcc2894d7293cc0
Giovane <giovaneaalmeida27@gmail.com>
2021-12-21 10:42:39 -0300

    feat: add function createTask

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { CreateTaskDTO } from './dtos/create-task.dto';
+import { Task, TaskStatus } from './tasks.model';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = [];
+
+  async createTask(createTaskDTO: CreateTaskDTO): Promise<Task> {
+    const task: Task = {
+      id: uuid(),
+      title: createTaskDTO.title,
+      description: createTaskDTO.description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+    return task;
+  }
+}
