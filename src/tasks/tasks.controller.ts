16cc33424ebf0ab67e3b149c952034fb43f2e51a
Giovane <giovaneaalmeida27@gmail.com>
2021-12-21 10:42:52 -0300

    feat: add route createTask

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller } from '@nestjs/common';
+import { CreateTaskDTO } from './dtos/create-task.dto';
+import { Task } from './tasks.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
+
+  async createTask(@Body() createTaskDTO: CreateTaskDTO): Promise<Task> {
+    return await this.tasksService.createTask(createTaskDTO);
+  }
 }
