6e1167821b92591d6199c75a4781f900e3a31fef
Giovane <giovaneaalmeida27@gmail.com>
2021-12-24 14:11:33 -0300

    feat: create function signUp

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDTO } from './dtos/auth-credentials.dto';
+import { UsersRepository } from './users.repository';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(UsersRepository)
+    private readonly usersRepository: UsersRepository,
+  ) {}
+
+  async signUp(authCredentialsDTO: AuthCredentialsDTO): Promise<void> {
+    await this.usersRepository.createUser(authCredentialsDTO);
+  }
+}
