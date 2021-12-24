9c70c1de8b642b4328832f153f5a70ed25e744b7
Giovane <giovaneaalmeida27@gmail.com>
2021-12-24 14:28:21 -0300

    feat: add validation to the parameters

--- src/auth/dtos/auth-credentials.dto.ts
+++ src/auth/dtos/auth-credentials.dto.ts
@@ -1,4 +1,23 @@
+import {
+  IsNotEmpty,
+  IsString,
+  Matches,
+  Max,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class AuthCredentialsDTO {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Password to weak',
+  })
   password: string;
 }
