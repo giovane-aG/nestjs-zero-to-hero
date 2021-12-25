76239e40c76c087791682d2a3a3ec8cfdd5f369f
Giovane <giovaneaalmeida27@gmail.com>
2021-12-25 13:57:22 -0300

    refact: move signIn responsability to auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
+import { compare } from 'bcrypt';
+import { AccessToken } from './access-token.interface';
 import { AuthCredentialsDTO } from './dtos/auth-credentials.dto';
+import { JwtPayload } from './jwt-payload.interface';
 import { UsersRepository } from './users.repository';
 
 @Injectable()
@@ -8,13 +12,30 @@ export class AuthService {
   constructor(
     @InjectRepository(UsersRepository)
     private readonly usersRepository: UsersRepository,
+    private readonly jwtService: JwtService,
   ) {}
 
   async signUp(authCredentialsDTO: AuthCredentialsDTO): Promise<void> {
     await this.usersRepository.createUser(authCredentialsDTO);
   }
 
-  async signIn(authCredentialsDTO: AuthCredentialsDTO): Promise<string> {
-    return await this.usersRepository.signIn(authCredentialsDTO);
+  async signIn(authCredentialsDTO: AuthCredentialsDTO): Promise<AccessToken> {
+    const { password, username } = authCredentialsDTO;
+    const user = await this.usersRepository.getUserByUsername(username);
+
+    if (!user) {
+      throw new BadRequestException(`Username/password is incorrect`);
+    }
+
+    const passwordMatches = await compare(password, await user.password);
+
+    if (!passwordMatches) {
+      throw new BadRequestException(`Username/password is incorrect`);
+    }
+
+    const payload: JwtPayload = { username };
+    const accessToken = await this.jwtService.sign(payload);
+
+    return { accessToken };
   }
 }
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,12 +1,8 @@
-import {
-  BadRequestException,
-  ConflictException,
-  NotFoundException,
-} from '@nestjs/common';
+import { ConflictException } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDTO } from './dtos/auth-credentials.dto';
 import { User } from './user.entity';
-import { hash, genSalt, compare } from 'bcrypt';
+import { hash, genSalt } from 'bcrypt';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
@@ -33,22 +29,5 @@ export class UsersRepository extends Repository<User> {
     user.password = hashedPassword;
 
     await this.save(user);
-  }
-
-  async signIn(authCredentialsDTO: AuthCredentialsDTO): Promise<string> {
-    const { password, username } = authCredentialsDTO;
-    const user = await this.getUserByUsername(username);
-
-    if (!user) {
-      throw new BadRequestException(`Username/password is incorrect`);
-    }
-
-    const passwordMatches = await compare(password, await user.password);
-
-    if (!passwordMatches) {
-      throw new BadRequestException(`Username/password is incorrect`);
-    }
-
-    return 'success';
   }
 }
