615043ac6de86b49e7b2723fc3e9d074d6006f26
Giovane <giovaneaalmeida27@gmail.com>
2021-12-25 10:29:29 -0300

    feat(usersRepository): create function signIn

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,8 +1,12 @@
-import { ConflictException } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  NotFoundException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDTO } from './dtos/auth-credentials.dto';
 import { User } from './user.entity';
-import { hash, genSalt } from 'bcrypt';
+import { hash, genSalt, compare } from 'bcrypt';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
@@ -29,5 +33,22 @@ export class UsersRepository extends Repository<User> {
     user.password = hashedPassword;
 
     await this.save(user);
+  }
+
+  async signIn(authCredentialsDTO: AuthCredentialsDTO): Promise<string> {
+    const { password, username } = authCredentialsDTO;
+    const user = await this.getUserByUsername(username);
+
+    if (!user) {
+      throw new BadRequestException(`Username/password is incorrect`);
+    }
+
+    const passwordMatches = await compare(password, await user.password);
+
+    if (!passwordMatches) {
+      throw new BadRequestException(`Username/password is incorrect`);
+    }
+
+    return 'success';
   }
 }
